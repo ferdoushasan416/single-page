3ebfde07fe5e0f0be89ef06443d1569725cbe737
Faisal6274 <faisaldevbhaluka@gmail.com>
2023-02-14 16:05:25 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+(function ($) {
+"use strict";
+
+// meanmenu
+$('#mobile-menu').meanmenu({
+	meanMenuContainer: '.mobile-menu',
+	meanScreenWidth: "992"
+});
+
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
